c49f30c0496bb4736ac0d36774f5f33d6ffa5271
kofikg <kofiivan86@gmail.com>
2023-06-25 17:07:15 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+#configure ssh config
+
 file_line{'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no'
@@ -7,3 +9,4 @@ file_line{'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school'
 }
+
